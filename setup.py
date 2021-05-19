b293dcaab20f699a3ca9c017d7fbadaa1030fac0
silasitttes <silastittes@gmail.com>
2021-05-18 21:34:39 -0600

    version update to pip

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = 'Produces bedfile of genomic locations that did or did not ma
   
 setup( 
         name ='mop-bam', 
-        version ='0.0.3.3', 
+        version ='0.0.4.0', 
         author ='Silas Tittes', 
         author_email ='silas.tittes@gmail.com', 
         url ='https://github.com/RILAB/mop', 
