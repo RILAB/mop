06e59f4a2d2fe82f2cdd3a692a13917b4a03aa01
silastittes <silas.tittes@gmail.com>
2021-05-18 19:14:45 -0700

    added a pixy mode and updated documentation for usage.

--- mop/mop.py
+++ mop/mop.py
@@ -17,29 +17,29 @@ def main():
     parser.add_argument("-s", "--bad_sites", action='store_true', 
                 help = "Switch to return sites which fail thresholds. Default is to return passing sites.")
 
+    parser.add_argument("--pixy_mode", action='store_true', 
+                help = "Switch to produce output for pixy (reports every site and the number of individuals with passing quality thresholds).")
+
     parser.add_argument("-M", "--mean_depth_min", type = int, default=0,
-                help = "Minimum mean depth across all individuals.")
+                help = "Minimum mean depth across all individuals (ignored with  pixy_mode).")
 
     parser.add_argument("-x", "--max_depth", type = float, default = float("inf"),
-                help = "Maximum number of bases allowed per individual after accounting for low base and mapping quality. This flag should always be used in conjunction with -m.")
+                help = "Maximum number of bases allowed per individual after accounting for low base and mapping quality. This flag should always be used in conjunction with -m (ignored with  pixy_mode).")
 
     parser.add_argument("-i", "--min_depth", type = int, default=0, 
                 help = "Minimum number of bases required per individual after accounting for low base and mapping quality. This flag should always be used in conjunction with -m.")
 
     parser.add_argument("-m", "--depth_proportion", type = float, default=0, 
-                help = "Minimum proportion of individuals with site counts greater than --min_depth that are required for site to pass. Test is applied after accounting for low base and mapping quality.")
+                help = "Minimum proportion of individuals with site counts greater than --min_depth that are required for site to pass. Test is applied after accounting for low base and mapping quality (ignored with  pixy_mode).")
 
     parser.add_argument("-Q", "--map_quality", type = int, default=0, 
-                help = "Minimim mapping quality")
+                help = "Minimim mapping quality.")
 
     parser.add_argument("-q", "--base_quality", type = int, default=0, 
-                help = "Minimim base quality")
+                help = "Minimim base quality.")
 
     parser.add_argument('-b', '--bamlist', nargs="?", type=str, required = True,
                 help='List of bam files. One per line.')
-
-    #parser.add_argument('-B', '--bedfile', nargs="?", type=argparse.FileType('w'), 
-    #		    help='Optional name of the file to write bedfile to.')
 
     parser.add_argument('-l', '--positions_file', type=str, 
                 help='Optional file of reference position to pass to "samtools depth".')
@@ -74,9 +74,24 @@ def main():
 
         return passing
 
+
     def printer(chrom, start, end):
         if start > 0 and  end > 0:
             print(f"{chrom}\t{start-1}\t{end}")
+
+    ### FOR PIXY MODE ###
+    def qual_count(parse_bam):
+        pop_bam = parse_bam['pop_bam']
+        idx = parse_bam['idx']
+        depth = np.array([int(pop_bam[i]) for i in idx])
+            
+        #number of individuals with depth greater than the user-specified minimum
+        n_ind = np.sum(depth >= args.min_depth) 
+        return n_ind
+
+    def pixy_printer(chrom, start, end, n_ind):
+        if start > 0 and  end > 0:
+            print(f"{chrom}\t{start-1}\t{end}\t{n_ind}")
 
 
     if args.positions_file:	
@@ -91,8 +106,22 @@ def main():
     end = -1
     init = True   
 
+
+    ### PIXY MODE
+    if args.pixy_mode:
+        proc = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+        for line in io.TextIOWrapper(proc.stdout, encoding="utf-8"):
+            line_dict = parse_line(line)
+            n_ind = qual_count(line_dict)
+            chrom = line_dict['chrom']
+            start = line_dict['pos']
+            end = line_dict['pos']
+            pixy_printer(chrom, start, end, n_ind)
+        quit()
+
+    ### GOOD OLD MOP
     proc = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-    for line in io.TextIOWrapper(proc.stdout, encoding="utf-8"):
+    for line in io.TextIOWrapper(proc.stdout, encoding="utf-8"): 
         line_dict = parse_line(line)
         test = qual_check(line_dict)
 
@@ -146,5 +175,4 @@ def main():
                             init = False
     printer(chrom, start, end)
 
-
-#main()
+main()
